95d61051d36211fe45f10524590e0686448db82c
ZaneleJ <tsotetsijacqueline@gmail.com>
2023-07-16 04:27:11 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		int i;
 
-	for (i = 0; i < 52; i++);
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
-	putchar('\n';
+	putchar('\n');
 	return (0);
 }
