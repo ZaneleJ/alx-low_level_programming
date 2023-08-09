2ec4db9e8af47e09838186b82e6aa70c1a8b0803
ZaneleJ <tsotetsijacqueline@gmail.com>
2023-08-08 20:19:23 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @size: size of array
  * Description: Creates an array of size 'size' and assigns the character 'c'
  * to each element of the array.
- * Return:Pointer to the created array, NULL if allocation fails or size is
+ * Return:Pointer to the created array, NULL if allocation fails or size is 0
  *
  */
 
@@ -19,6 +19,7 @@ char *create_array(unsigned int size, char c)
 
 
 	str = malloc(sizeof(char) * size);
+
 	if (size == 0 || str == NULL)
 		return (NULL);
 
